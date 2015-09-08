05962e3db9d09aafe0af2d0ccced7e5af11ec84a
Marvin Bertin <marvin.bertin@gmail.com>
2015-09-07 22:04:45 -0400

    Laplace Unigram Language Model completed

--- Week2/CMP462 HW02 Data/python/LaplaceUnigramLanguageModel.py
+++ Week2/CMP462 HW02 Data/python/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,33 @@
+from collections import defaultdict
+from math import log
+
 class LaplaceUnigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.UnigramCounts = defaultdict(int)
+    self.total = 0
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+        for datum in sentence.data:
+            token = datum.world
+            self.UnigramCounts[token] += 1
+            self.total += 1
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
+        Plus one smoothing.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0
+    V = len(self.UnigramCounts)
+    for token in sentence:
+        count = self.UnigramCounts[token]
+        score += log(count + 1)
+        score -= log(self.total + V)
+    return score
