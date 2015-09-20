6631739b94975ebd5cb7a69764948722ca663014
Marvin Bertin <marvin.bertin@gmail.com>
2015-09-20 18:03:17 -0400

    fixing layout

--- Week3/CMP462 HW03 Data/python/NaiveBayes.py
+++ Week3/CMP462 HW03 Data/python/NaiveBayes.py
@@ -1,16 +1,5 @@
 # NLP Programming Assignment #3
 # NaiveBayes
-# 2012
-
-#
-# The area for you to implement is marked with TODO!
-# Generally, you should not need to touch things *not* marked TODO
-#
-# Remember that when you submit your code, it is not run from the command line
-# and your main() will *not* be run. To be safest, restrict your changes to
-# addExample() and classify() and anything you further invoke from there.
-#
-
 
 import sys
 import getopt
